649e20b542fcb1e2f79b2c45fc642a4242babeab
tec-armando <116966469+tec-armando@users.noreply.github.com>
2023-01-25 19:35:42 -0300

    Atualização de de projeto

--- assets/script/add-class.js
+++ assets/script/add-class.js
@@ -1,22 +1,26 @@
-// let scroll =document.scrollingElement.addEventListener('scroll', scrollX);
-// let scrollX = document.scrollingElement.scrollLeft;
-// console.log(scrollX);
+window.addEventListener("scroll", positionX);
 
 function positionX() {
+  let scrollX = document.scrollingElement.scrollTop;
+
+  if(scrollX > 1) {
   addClass();
+  } else if(scrollX < 1) {
+  removeClass();
+  }
 }
 
-let image = document.querySelector(".header__container-image")
 function addClass() {
-  let scrollX = document.scrollingElement.scrollTop;
-  console.log(scrollX);
+  if(window.innerWidth > 768) return false;
+  let image = document.querySelector(".header__image")
   let header = document.querySelector('.header');
-  if(scrollX > 1) {
   header.classList.add('header-fixed');
-  image.classList.add('header__img-fixed');
-  } else if(scrollX < 1) {
+  image.classList.add('header__img-fixed'); 
+}
+
+function removeClass() {
+  let image = document.querySelector(".header__image")
+  let header = document.querySelector('.header');
   header.classList.remove('header-fixed');
   image.classList.remove('header__img-fixed');
-  }
- 
 }
